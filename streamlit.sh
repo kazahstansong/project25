c139dc553c0d4ac7a9c7090cfdd9c6dd592cd801
ssong <kazahstansong@gmail.com>
2025-10-30 17:05:35 +0900

    변경

---
+++ streamlit.sh
@@ -0,0 +1,15 @@
+pip install streamlit
+pip install openai
+pip install python-dotenv
+pip install langchain 
+pip install langchain-openai
+pip install langchain-community
+pip install azure-search-documents
+pip install tiktoken
+pip install openai
+pip install azure-identity
+pip install requests
+pip install json5
+pip install pathlib
+
+python -m streamlit run main.py --server.port 8000 --server.address 0.0.0.0
